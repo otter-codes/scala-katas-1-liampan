957ee8b0a56ca3587f673efd91258fd98c79f606
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-02-01 16:53:10 +0000

    index vals done

---
+++ src/main/scala/App/IndexValues.scala
@@ -0,0 +1,16 @@
+package App
+
+object IndexValues extends App {
+
+  val TheList = List(2, 4, 7, 9, 10)
+
+
+  val refined = TheList.zipWithIndex
+                        .tail
+                        .filter(x => x._1 % x._2 == 0)
+                        .map(x => x._1)
+
+println(refined)
+
+  // with the assistance nick
+}
